459f1254a0edcb4a7fec70c008e93beb4a2a9b46
fitryannugrahaa <32404079+fitryannugrahaa@users.noreply.github.com>
2017-10-25 12:01:36 +0800

    Praktek 0204

--- Untitled5.cpp
+++ Untitled5.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
 int main(){
 	
+	int LR=42;
+	cout<<"Luas Rumah				:"<<LR<<" m2 "<<endl;
+	double LP=1.87596;
+	cout<<"Luas 1 box parket		:"<<LP<<" m2 "<<endl;
+	double JP=LR/LP;
+	cout<<"Jumlah box parket		:"<<JP<<endl;
+	double BPR=JP*500;
+	cout<<"Biaya parker (Rp)		:"<<BPR<<endl;
+	int BPS=LR*20;
+	cout<<"Biaya pasang (Rp)		:"<<BPS<<endl;
+	double TB=BPR+BPS;
+	cout<<"Total Niaya	(Rp)		:"<<TB<<endl;
+	cout<<"-------------------------------------";
+	getch();
 }
